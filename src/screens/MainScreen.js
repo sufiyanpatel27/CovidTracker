aed47cc749bf4f10602faa0ea403ec4d3e5a0996
sufiyan <sp3596894@gmail.com>
2021-05-13 16:52:49 +0530

    half mainscreen completed

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,6 +5,16 @@ import { useLocation } from "react-router-dom";
 //firebse
 import firebase from 'firebase'
 
+//assets
+import home from '../assets/home.png'
+import calendar from '../assets/calendar.png'
+import chat from '../assets/chat.png'
+import user from '../assets/user.png'
+import settings from '../assets/settings.png'
+import healthcare from '../assets/healthcare.png'
+import no_beds from '../assets/no_beds.png'
+import no_oxygen from '../assets/no_oxygen.png'
+import no_vaccines from '../assets/no_vaccines.png'
 
 export default function MainScreen() {
     const location = useLocation();
@@ -14,9 +24,36 @@ export default function MainScreen() {
     })
 
     return (
-        <div>
-            <h1>main screen</h1>
-            <h6>{location.username}</h6>
+        <div className="Application">
+            <div className="navbar">
+                <div className='logo_container'>
+                    <img src={healthcare} alt='home' />
+                </div>
+                <div className="navbar_container">
+                    <img src={home} alt='home' className='small_icons' />
+                    <img src={calendar} alt='home' />
+                    <img src={chat} alt='home' />
+                    <img src={user} alt='home' />
+                    <img src={settings} alt='home' />
+                </div>
+            </div>
+            <div className="covidcare_container">
+                <div className="mainscreen_upperbar">
+                    <h1 style={{ color: '#585858' }}>Hello, There</h1>
+                    <h6 style={{ color: '#B5B5B6' }}>Stay Home, Stay Safe</h6>
+                </div>
+                <div className="mainscreen_main_content">
+                    <img src={no_beds} alt='no_beds' />
+                    <img src={no_oxygen} alt='no_beds' />
+                    <img src={no_vaccines} alt='no_beds' />
+                </div>
+                <div className="mainscreen_main_graph">
+
+                </div>
+            </div>
+            <div className="covidcare_container2">
+                second
+            </div>
         </div>
     )
 }
